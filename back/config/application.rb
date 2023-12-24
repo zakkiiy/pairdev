bdb379a940fb91ddf521eae91b15d82715fa7dc4
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2023-12-24 18:19:27 +0900

    rails 環境構築及びrails s完了

--- back/config/application.rb
+++ back/config/application.rb
@@ -23,5 +23,8 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
   end
 end
