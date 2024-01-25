f44d82efd0c7bb8a85cd5af88e6c7c176c9efb50
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2024-01-25 13:36:33 +0900

    [fix][本番エラー]ActionCableに伴うconfig変更_5

--- back/config/environments/production.rb
+++ back/config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://pairdev.onrender.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://pairdev.vercel.app', 'https://pairdev.vercel.app/' ]
+  config.action_cable.allowed_request_origins = [ 'https://pairdev.vercel.app', 'https://pairdev.vercel.app/', 'http://pairdev.vercel.app', 'http://pairdev.vercel.app/' ]
   config.action_cable.disable_request_forgery_protection = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
