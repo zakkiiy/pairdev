f7eef3676ed6ad761f354d80239e86cf7039b3e6
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2024-01-25 13:24:07 +0900

    [fix][本番エラー]ActionCableに伴うconfig変更_4

--- back/config/environments/production.rb
+++ back/config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   config.action_cable.disable_request_forgery_protection = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = false
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
