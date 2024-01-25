238f36f1d2a12a5b60e0da5e1fcaa9d1e3ed58f4
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2024-01-25 14:37:51 +0900

    [fix][本番エラー]ActionCableに伴うconfig変更_10

--- back/config/initializers/cors.rb
+++ back/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', '127.0.0.1:3000', 'https://pairdev.vercel.app', 
+    origins 'localhost:3000', '127.0.0.1:3000', 'https://pairdev.vercel.app'
 
     resource "*",
       headers: :any,
