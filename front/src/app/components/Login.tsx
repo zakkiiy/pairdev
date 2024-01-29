2b2592f45a148bbfba43960186c18a38ff9fe98d
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2024-01-29 20:28:17 +0900

    [fix]ログイン前のGitHubアイコン追加

--- front/src/app/components/Login.tsx
+++ front/src/app/components/Login.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSession, signIn } from 'next-auth/react';
+import { FaGithub } from 'react-icons/fa';
 
 export default function Login() {
 	const { data: session, status } = useSession();
@@ -9,14 +10,16 @@ export default function Login() {
 	}
 
 	if (status !== 'authenticated') {
-		return (
-			<div>
-				<p>以下からログインしてください。</p>
-				<button onClick={() => signIn('github', {}, { prompt: 'login' })}>
-					GitHubでログイン
-				</button>
-			</div>
-		);
-	}
+    return (
+      <div className="text-center">
+        <button
+          onClick={() => signIn('github')}
+          className="bg-gray-800 text-white px-4 py-2 rounded flex items-center justify-center w-full md:w-auto"
+        >
+          <FaGithub className="mr-2" /> GitHub
+        </button>
+      </div>
+    );
+  }
 	return null;
 }
