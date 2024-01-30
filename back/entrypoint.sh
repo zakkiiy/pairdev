92d0a97434b2329046ad9f11d6b22093d1d4c209
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2024-01-30 15:09:17 +0900

    [fix]render設定2

--- back/entrypoint.sh
+++ back/entrypoint.sh
@@ -5,9 +5,7 @@ set -e
 rm -f /myapp/tmp/pids/server.pid
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
+bundle exec rake db:drop db:create db:migrate
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
