ccaca48ca6899c40442825b2f1a4993c8b2df80a
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2024-01-30 14:00:50 +0900

    [fix]関連付け削除

--- back/app/models/user.rb
+++ back/app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
-  has_many :room_users
+  has_many :room_users, dependent: :destroy
   has_many :rooms, through: :room_users
   has_many :messages
-  has_one :profile
+  has_one :profile, dependent: :destroy
 
   after_create :create_default_profile
 
