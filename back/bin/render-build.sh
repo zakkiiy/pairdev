68df0472fde0fd29281a7306e76f99418ca037ff
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2024-01-30 15:00:50 +0900

    [fix]render設定

--- back/bin/render-build.sh
+++ back/bin/render-build.sh
@@ -1,5 +1,9 @@
+bin/render-build.sh内
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-# bundle exec rake db:migrate ←コメントアウト
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
