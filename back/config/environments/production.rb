39c5d3d7410ffa5e571fc2f1579b067a6b61bd57
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2024-07-30 00:20:10 +0900

    修正

--- back/config/environments/production.rb
+++ back/config/environments/production.rb
@@ -88,5 +88,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.hosts << "pairdev-dlw9.onrender.com"
+  config.hosts << "pairdev-pro.onrender.com"
 end
